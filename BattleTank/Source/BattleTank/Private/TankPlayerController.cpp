a48ba45574d8ac00a107378234a9ea371c3adbc2
Rafael Alfaro <rafalfaro_18@outlook.com>
2019-06-30 01:33:58 -0600

    Override beginplay method and log name of controlled tank

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -6,3 +6,12 @@
 ATank* ATankPlayerController::GetControlledTank() const {
 	return Cast<ATank>(GetPawn());
 }
+
+void ATankPlayerController::BeginPlay(){
+	Super::BeginPlay();
+	UE_LOG(LogTemp, Warning, TEXT("PlayerController Begin Play"));
+
+	if (GetControlledTank() != nullptr) {
+		UE_LOG(LogTemp, Warning, TEXT("Tank %s controlled"), *GetControlledTank()->GetName());
+	}
+}
