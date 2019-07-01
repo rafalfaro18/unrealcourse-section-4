e8066173fecd92b6f533f209af1ca73a97f7c216
Rafael Alfaro <rafalfaro_18@outlook.com>
2019-06-30 23:13:30 -0600

    tankAIcontroller gets player tank

--- BattleTank/Source/BattleTank/Private/TankAIController.cpp
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -1,6 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
+#include "Classes/Engine/World.h"
+#include "Classes/GameFramework/PlayerController.h"
 #include "TankAIController.h"
 
 ATank* ATankAIController::GetControlledTank() const {
@@ -18,4 +20,24 @@ void ATankAIController::BeginPlay() {
 	else {
 		UE_LOG(LogTemp, Warning, TEXT("TankAIController possesing: %s"), *ControlledTank->GetName());
 	}
+
+	auto PlayerTank = GetPlayerTank();
+
+	if (!PlayerTank) {
+		UE_LOG(LogTemp, Warning, TEXT("TankAIController can't find player tank."));
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("TankAIController found player tank: %s"), *PlayerTank->GetName());
+	}
+}
+
+ATank * ATankAIController::GetPlayerTank() const
+{
+	auto PlayerPawn = GetWorld()->GetFirstPlayerController()->GetPawn();
+
+	if (!PlayerPawn) {
+		return nullptr;
+	}
+	
+	return Cast<ATank>(PlayerPawn);
 }
