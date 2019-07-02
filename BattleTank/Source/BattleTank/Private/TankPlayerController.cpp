12843a100e17ab50916883636b3623de47c2a7cd
Rafael Alfaro <rafalfaro_18@outlook.com>
2019-07-01 20:28:43 -0600

    Adds pseudo code

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -18,7 +18,7 @@ void ATankPlayerController::BeginPlay(){
 void ATankPlayerController::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-	// AimTowardsCrosshair();
+	AimTowardsCrosshair();
 	UE_LOG(LogTemp, Warning, TEXT("PlayerController ticking."));
 }
 
@@ -26,4 +26,12 @@ void ATankPlayerController::Tick(float DeltaTime)
 
 ATank* ATankPlayerController::GetControlledTank() const {
 	return Cast<ATank>(GetPawn());
+}
+
+void ATankPlayerController::AimTowardsCrosshair() {
+	if (!GetControlledTank()) { return; }
+
+	// Get world location if linetrace through crosshair
+	// If it hits the landscape
+		// Tell controlled tank to aim at this point
 }
