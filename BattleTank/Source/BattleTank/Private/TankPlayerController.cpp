1236dfdb288798d9485754688093175154c16ea1
Rafael Alfaro <rafalfaro_18@outlook.com>
2019-07-02 22:38:30 -0600

    update comment

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -34,7 +34,7 @@ void ATankPlayerController::AimTowardsCrosshair() {
 
 	FVector HitLocation;// Out parameter
 	if (GetSightRayHitLocation(HitLocation)) { // Has "side-effect", is going to line trace
-		UE_LOG(LogTemp, Warning, TEXT("Look direction: %s"), *HitLocation.ToString());
+		UE_LOG(LogTemp, Warning, TEXT("Hit location: %s"), *HitLocation.ToString());
 		// TODO: Tell controlled tank to aim at this point.
 	}
 }
