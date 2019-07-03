f5c21b5849d90d8f6d0018b8c6db8619bea059fb
Rafael Alfaro <rafalfaro_18@outlook.com>
2019-07-02 20:57:01 -0600

    Logs crosshair position

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -32,7 +32,7 @@ void ATankPlayerController::AimTowardsCrosshair() {
 
 	FVector HitLocation;// Out parameter
 	if (GetSightRayHitLocation(HitLocation)) { // Has "side-effect", is going to line trace
-		UE_LOG(LogTemp, Warning, TEXT("Look direction: %s"), *HitLocation.ToString());
+		// UE_LOG(LogTemp, Warning, TEXT("Look direction: %s"), *HitLocation.ToString());
 		// TODO: Tell controlled tank to aim at this point.
 	}
 }
@@ -43,6 +43,9 @@ bool ATankPlayerController::GetSightRayHitLocation(FVector& OutHitLocation) cons
 	// Find the crosshair positon
 	int32 ViewportSizeX, ViewportSizeY;
 	GetViewportSize(ViewportSizeX, ViewportSizeY);
+
+	auto ScreenLocation = FVector2D(ViewportSizeX * CrosshairXLocation, ViewportSizeY * CrosshairYLocation);
+	UE_LOG(LogTemp, Warning, TEXT("ScreenLocation: %s"), *ScreenLocation.ToString());
 
 	// "De-project" the screen position of the crosshair to a world direction
 	// Line trace along that look direction, and see what we hit (up to max range)
--- BattleTank/Source/BattleTank/Public/TankPlayerController.h
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -17,16 +17,21 @@ class BATTLETANK_API ATankPlayerController : public APlayerController
 	GENERATED_BODY()
 
 public:
-	ATank* GetControlledTank() const;
 	virtual void BeginPlay() override;
 	virtual void Tick(float DeltaTime) override;
 
+private:
+	ATank* GetControlledTank() const;
 	// Start the tank moving the barrel so that a shot would hit where
 	// the crosshair intersects the world.
 	void AimTowardsCrosshair();
 
-private:
-
 	// Return an OUT parameter, true if hit landscape
 	bool GetSightRayHitLocation(FVector& OutHitLocation) const;
+
+	UPROPERTY(EditAnywhere)
+	float CrosshairXLocation = 0.5f;
+
+	UPROPERTY(EditAnywhere)
+	float CrosshairYLocation = 0.3333f;
 };
