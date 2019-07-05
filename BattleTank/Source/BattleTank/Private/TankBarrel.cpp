8274a3d8db182569b6973b9e0da134339dbb36ae
Rafael Alfaro <rafalfaro_18@outlook.com>
2019-07-05 01:11:18 -0600

    log elapse time at barrel elevate()

--- BattleTank/Source/BattleTank/Private/TankBarrel.cpp
+++ BattleTank/Source/BattleTank/Private/TankBarrel.cpp
@@ -6,5 +6,6 @@
 void UTankBarrel::Elevate(float RelativeSpeed) {
 	// Move the barrel the right amount this frame
 	// Given a max elevation speed, and the frame time
-	UE_LOG(LogTemp, Warning, TEXT("Barrel->Elevate() called at speed %f"), RelativeSpeed);
+	auto Time = GetWorld()->GetTimeSeconds();
+	UE_LOG(LogTemp, Warning, TEXT("%f: Barrel->Elevate() called at speed %f"), Time, RelativeSpeed);
 }
