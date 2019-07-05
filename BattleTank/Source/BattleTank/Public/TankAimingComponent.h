ecd60495d1b4f11e67be9069881363cf2675995b
Rafael Alfaro <rafalfaro_18@outlook.com>
2019-07-05 00:20:22 -0600

    Adds elevation setup settings to TankBarrel component

--- BattleTank/Source/BattleTank/Public/TankAimingComponent.h
+++ BattleTank/Source/BattleTank/Public/TankAimingComponent.h
@@ -7,8 +7,10 @@
 #include "Components/ActorComponent.h"
 #include "TankAimingComponent.generated.h"
 
-class UTankBarrel; // Forward declaration
+// Forward declaration
+class UTankBarrel;
 
+// Hold barrel's properties and Elevate method.
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class BATTLETANK_API UTankAimingComponent : public UActorComponent
 {
