ac5e38708ccc900bb30b082a3e73e5485d9b0e55
Rafael Alfaro <rafalfaro_18@outlook.com>
2019-07-07 20:40:29 -0600

    refactor

--- BattleTank/Source/BattleTank/Private/TankAIController.cpp
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -6,46 +6,23 @@
 #include "Classes/GameFramework/PlayerController.h"
 #include "Tank.h"
 
-ATank* ATankAIController::GetControlledTank() const {
-	return Cast<ATank>(GetPawn());
-}
-
 void ATankAIController::BeginPlay() {
 	Super::BeginPlay();
-	auto ControlledTank = GetControlledTank();
-	UE_LOG(LogTemp, Warning, TEXT("TankAIController Begin Play"));
-
-	auto PlayerTank = GetPlayerTank();
-
-	if (!PlayerTank) {
-		UE_LOG(LogTemp, Warning, TEXT("TankAIController can't find player tank."));
-	}
-	else {
-		UE_LOG(LogTemp, Warning, TEXT("TankAIController found player tank: %s"), *PlayerTank->GetName());
-	}
-}
-
-ATank * ATankAIController::GetPlayerTank() const
-{
-	auto PlayerPawn = GetWorld()->GetFirstPlayerController()->GetPawn();
-
-	if (!PlayerPawn) {
-		return nullptr;
-	}
-	
-	return Cast<ATank>(PlayerPawn);
 }
 
 void ATankAIController::Tick(float DeltaSeconds)
 {
 	Super::Tick(DeltaSeconds);
-	if (GetPlayerTank()) {
+	auto PlayerTank = Cast<ATank>(GetWorld()->GetFirstPlayerController()->GetPawn());
+	auto ControlledTank = Cast<ATank>(GetPawn());
+
+	if (PlayerTank) {
 		// TODO: Move towards the player
 
 		// Aim towards the player
-		GetControlledTank()->AimAt(GetPlayerTank()->GetActorLocation());
+		ControlledTank->AimAt(PlayerTank->GetActorLocation());
 
 		// Fire if ready
-		GetControlledTank()->Fire();
+		ControlledTank->Fire(); // TODO: limit firing rate
 	}
 }
--- BattleTank/Source/BattleTank/Public/TankAIController.h
+++ BattleTank/Source/BattleTank/Public/TankAIController.h
@@ -17,10 +17,8 @@ class BATTLETANK_API ATankAIController : public AAIController
 	GENERATED_BODY()
 
 private:
-	ATank* GetControlledTank() const;
-	virtual void BeginPlay() override;
 
-	ATank* GetPlayerTank() const;
+	virtual void BeginPlay() override;
 
 	virtual void Tick(float DeltaSeconds) override;
 };
