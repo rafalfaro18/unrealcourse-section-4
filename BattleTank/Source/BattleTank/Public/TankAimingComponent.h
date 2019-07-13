08fc8816f211ac6744f35ad18f4e2ace5e1974b2
Rafael Alfaro <rafalfaro_18@outlook.com>
2019-07-13 15:22:06 -0600

    makes Aiming the default firing state

--- BattleTank/Source/BattleTank/Public/TankAimingComponent.h
+++ BattleTank/Source/BattleTank/Public/TankAimingComponent.h
@@ -38,7 +38,7 @@ public:
 protected:
 
 	UPROPERTY(BlueprintReadOnly, Category = "State")
-	EFiringState FiringState = EFiringState::Reloading;
+	EFiringState FiringState = EFiringState::Aiming;
 
 private:
 
