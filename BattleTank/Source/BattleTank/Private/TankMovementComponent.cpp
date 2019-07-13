775e22f68d03a6709350bda1f21352fbf3dad0f2
Rafael Alfaro <rafalfaro_18@outlook.com>
2019-07-13 11:57:37 -0600

    Refactor

--- BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright Rafalfaro.
 
 
 #include "TankMovementComponent.h"
@@ -27,12 +27,10 @@ void UTankMovementComponent::IntendMoveForward(float Throw) {
 	if (!LeftTrack || !RightTrack) { return; }
 	LeftTrack->SetThrottle(Throw);
 	RightTrack->SetThrottle(Throw);
-	// TODO: prevent double-speed due to dual control use.
 }
 
 void UTankMovementComponent::IntendTurnRight(float Throw) {
 	if (!LeftTrack || !RightTrack) { return; }
 	LeftTrack->SetThrottle(Throw);
 	RightTrack->SetThrottle(-Throw);
-	// TODO: prevent double-speed due to dual control use.
 }
