d2f2918864155c9aae905c0f6c6c4b4214f8c677
Rafael Alfaro <rafalfaro_18@outlook.com>
2019-07-14 14:36:16 -0600

    Protect pointers with ensure macro

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -27,8 +27,9 @@ void ATank::BeginPlay() {
 
 void ATank::Fire()
 {
+	if (!ensure(Barrel)) { return; }
 	bool isReloaded = (FPlatformTime::Seconds() - LastFireTime) > ReloadTimeInSeconds;
-	if (Barrel && isReloaded) {
+	if (isReloaded) {
 		// Spawn a projectile at the socket location on the barrel.
 		auto Projectile = GetWorld()->SpawnActor<AProjectile>(
 			ProjectileBlueprint,
@@ -42,6 +43,6 @@ void ATank::Fire()
 }
 
 void ATank::AimAt(FVector HitLocation) {
-	if (!TankAimingComponent) { return; }
+	if (!ensure(TankAimingComponent)) { return; }
 	TankAimingComponent->AimAt(HitLocation, LaunchSpeed);
 }
