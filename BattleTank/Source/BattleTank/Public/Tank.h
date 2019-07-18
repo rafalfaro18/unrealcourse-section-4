5fdd544cef77a2f140c6cde49c178d275e909c23
Rafael Alfaro <rafalfaro_18@outlook.com>
2019-07-17 23:06:33 -0600

    Sets reference to tank at health bar widget

--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -11,14 +11,18 @@ class BATTLETANK_API ATank : public APawn
 {
 	GENERATED_BODY()
 
-	ATank();
-
 public:
 
 	// Called by the engine when actor damage is dealt
 	virtual float TakeDamage (float DamageAmount, struct FDamageEvent const & DamageEvent, class AController * EventInstigator, AActor * DamageCauser) override;
 
+	// Return current health as a percentage of starting health, between 0 and 1
+	UFUNCTION(BlueprintPure, Category = "Health")
+	float GetHealthPercent() const;
+
 private:
+	
+	ATank();
 
 	UPROPERTY(EditDefaultsOnly, Category = "Setup")
 	int32 StartingHealth = 100;
