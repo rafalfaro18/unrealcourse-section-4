b4a15b3bdd87174a47ec590ba6f9044e23b9ed63
Rafael Alfaro <rafalfaro_18@outlook.com>
2019-07-18 21:28:10 -0600

    broadcast tank's death

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -23,7 +23,7 @@ float ATank::TakeDamage(float DamageAmount, FDamageEvent const & DamageEvent, AC
 
 	CurrentHealth -= DamageToApply;
 	if (CurrentHealth <= 0) { 
-		UE_LOG(LogTemp, Warning, TEXT("Tank Died"));
+		OnDeath.Broadcast();
 	}
 
 	return DamageToApply;
