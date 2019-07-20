7ebd79a81b018f04827103367478632a0ee62fa4
Rafael Alfaro <rafalfaro_18@outlook.com>
2019-07-19 22:40:47 -0600

    Adds new SprungWheel class

---
+++ BattleTank/Source/BattleTank/Private/SprungWheel.cpp
@@ -0,0 +1,27 @@
+// Copyright Rafalfaro
+
+
+#include "SprungWheel.h"
+
+// Sets default values
+ASprungWheel::ASprungWheel()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ASprungWheel::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ASprungWheel::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ BattleTank/Source/BattleTank/Public/SprungWheel.h
@@ -0,0 +1,26 @@
+// Copyright Rafalfaro
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SprungWheel.generated.h"
+
+UCLASS()
+class BATTLETANK_API ASprungWheel : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ASprungWheel();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
